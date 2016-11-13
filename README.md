0ad9e360c9e6a680932375900d1cf459b67a67a6
Avin Grape <carcinogen75@gmail.com>
2016-11-13 14:13:32 +0300

    server tile transfer

--- README.md
+++ README.md
@@ -14,4 +14,19 @@ npm run start
 To try the tile server go to `http://localhost:4000/map/`
 
 ## Using
-...
+Use tile server url like this `http://localhost:4000/tiles/{s}.tile.stamen.com/toner/{z}/{x}/{y}.png?__protocol=http`
+it will get `http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png` tile 
+
+## Additional query params
+* `__protocol` - `[http|https]` (`http` - default) - protocol for tile server
+* `__nocache-primary` - `[true|false]` (`false` - default) - Try get tile image from original source first and if it will be unavailable - use cached copy    
+* `__nocache-only` - `[true|false]` (`false` - default) - Don't use cache source and always get tile image from original source.
+* `__filter` - `[gs|border]` - use tile image filter (can be an array).
+    * `gs` - gray-scale filter.
+    * `border` - draw tiles border.
+    
+## Example url
+```
+http://localhost:4000/tiles/{s}.tile.stamen.com/toner/{z}/{x}/{y}.png?__protocol=http&__nocache-primary=true
+http://localhost:4000/tiles/vec03.maps.yandex.net/tiles?l=map&v=4.127.2&x={x}&y={y}&z={z}&scale=1&lang=ru_RU&__protocol=https
+```
--- server/src/tiles.js
+++ server/src/tiles.js
@@ -1,17 +1,42 @@
 import KoaRouter from 'koa-router';
+import request from 'requestretry';
+import _ from 'lodash';
+import querystring from 'querystring';
 
 const routes = KoaRouter();
 
-routes.get('/:url',
-    async (ctx, next) => {
+routes.get('/:url*',
+    async(ctx, next) => {
         const {
             url
         } = ctx.params;
 
-        ctx.body = {
-            url: url,
-        };
-    });
+        const query = ctx.request.query;
 
+        const protocol = _.get(query, '__protocol', 'http');
+        const nocachePrimary = _.get(query, '__nocache-primary', false);
+        const nocacheOnly = _.get(query, '__nocache-only', false);
+        const filter = _.get(query, '__filter');
+        delete query['__protocol'];
+        delete query['__nocache-primary'];
+        delete query['__nocache-only'];
+        delete query['__filter'];
+
+        const queryStr = querystring.stringify(query);
+
+        //Get tile
+        const tileResponse = await request({
+            url: `${protocol}://${url}?${queryStr}`,
+            encoding: null, // Don't encode body
+            maxAttempts: 5, // try 5 times
+            retryDelay: 500, // wait for 5s before trying again
+            fullResponse: true // To resolve the promise with the full response
+        });
+
+        ctx.response.type = 'image/png';
+        ctx.response.set(tileResponse.headers);
+        ctx.body = tileResponse.body;
+
+    });
 
 export default routes;
